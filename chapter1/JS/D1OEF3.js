7ffeef91e793c961ace97558cc35b014fd18f3b8
olci6 <12000503@student.pxl.be>
2021-02-26 08:40:38 +0100

    dag 2

---
+++ chapter1/JS/D1OEF3.js
@@ -0,0 +1,3 @@
+let getal = prompt('Kies een getal tussen 1 en 5.', );
+let tekst = 'Gekozen getal: '+ getal;
+document.getElementById('rndGetal').innerHTML = tekst;
---
+++ chapter1/JS/oef test.js
@@ -0,0 +1,3 @@
+let code = prompt('Vul uw promotiecode in','uw code');
+let tekst = 'De code die u invoerde was: '+ code;
+document.getElementById('Kortingscode').innerHTML = tekst;
